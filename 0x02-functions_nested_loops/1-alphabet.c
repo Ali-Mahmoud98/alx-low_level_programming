a76541b670f3249799993bf6e3b153cce1fdf500
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-06-20 15:28:57 +0300

    Hot fix!

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,4 @@
-#include <stdio.h>
-
-/*Function that print alphabet*/
-/*print_alphabet - declaration*/
-void print_alphabet(void);
-/**
-  * main - print string
-  * Return: return (0) means success
-  */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
+#include "main.h"
 /**
   * print_alphabet - printing
   * chars from 'a' to 'z'
@@ -24,7 +11,7 @@ void print_alphabet(void)
 
 	while (a <= 'z')
 	{
-		putchar(a);
+		_putchar(a);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,34 +1,4 @@
-#include <stdio.h>
-
-/*Start Functions prototypes*/
-void print_alphabet(void);
-void print_alphabet_x10(void);
-/*End Functions prototypes*/
-/**
-  * main - print string
-  * Return: return (0) means success
-  */
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
-/**
-  * print_alphabet - printing
-  * chars from 'a' to 'z'
-  * takes no arguments
-  * Retuen: void
-  */
-void print_alphabet(void)
-{
-	char a = 'a';
-
-	while (a <= 'z')
-	{
-		putchar(a);
-	}
-	putchar('\n');
-}
+#include "main.h"
 /**
   * print_alphabet_x10 - calls the
   * function print_alphabet 10 times
