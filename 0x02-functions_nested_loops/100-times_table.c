a7e0069ca213ab5fe2eb79c25df18539de66b57b
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-06-20 10:48:54 -0700

    .

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,45 +1,38 @@
 #include "main.h"
 /**
-  * print_commaAndSpace - print
-  * @a: integer
-  */
-void print_commaAndSpace(int a)
-{
-	char x = 0;
-
-	_putchar(',');
-	while (x < a)
-		_putchar(' ');
-}
-/**
   * print_times_table - prints the n times table, starting with 0.
   * @n: integer
   */
 void print_times_table(int n)
 {
-	char i = 0, j;
+	char i = 0;
 
-	while (i <= n)
+	while ((i <= n) && (n <= 15) && (n >= 0))
 	{
-		if ((n > 15) && (n < 0))
-			break;
-		j = 0;
+		char j = 0;
+
 		while (j <= n)
 		{
 			if (j == 0)
 				_putchar('0' + (i * j));
 			if ((j != 0) && ((i * j) <= 9))
 			{
-				print_commaAndSpace(3);
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
 				_putchar('0' + (i * j));
-			} else if ((j != 0) && ((i * j) > 9) && ((i * j) < 100))
+			} else if ((j != 0) && ((i * j) < 99) && ((i * j) > 9))
 			{
-				print_commaAndSpace(2);
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 				_putchar('0' + ((i * j) / 10));
 				_putchar('0' + ((i * j) % 10));
 			} else if ((j != 0) && ((i * j) > 99))
 			{
-				print_commaAndSpace(1);
+				_putchar(',');
+				_putchar(' ');
 				_putchar('0' + ((i * j) / 100));
 				_putchar('0' + (((i * j) / 10) % 10));
 				_putchar('0' + ((i * j) % 10));
