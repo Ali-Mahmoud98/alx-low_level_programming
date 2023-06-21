42fef953dbe4035a8f757407e837b997643fa9a3
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-06-21 01:41:51 -0700

    second commit

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * main - causes an infinite loop
+  * Return: 0
+  */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 10;
+	//when i=0 infinit loop will occure 
+	while (i < 10)
+	{
+		putchar(i);
+	}
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
