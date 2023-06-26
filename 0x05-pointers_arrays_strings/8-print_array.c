92febbe068ac2b0c3450513ecfdd9d530adc43a6
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-06-26 06:32:34 -0700

    .

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * print_array - printing arrays elements
+  * @a: array name
+  * @n: number of array elements
+  */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+		printf("%d, ", *(a + i));
+	printf("%d\n", *(a + i));
+}
