7331f0adb7808e76fc0c7fd3fd9c692d3e0d95e8
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-06-29 04:22:16 -0700

    .

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * _strcat - concatinate two string
+  * @dest: string to be expanded
+  * @src: strint to be added to dest
+  * Return: string dest+src
+  */
+char *_strcat(char *dest, char *src)
+{
+	char len = 0;
+
+	while (dest[len] != '\0')
+		len++;
+
+	while (*src != '\0')
+	{
+		dest[len] = *src;
+		src++;
+		len++;
+	}
+	dest[len] = '\0';
+
+	return (dest);
+}
