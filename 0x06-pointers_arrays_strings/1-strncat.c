1299e9942d9f6477fbbf6dcf12989c62db06c9a7
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-06-29 04:43:01 -0700

    .

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * _strncat - copy number of n letters from string to another
+  * @dest: string
+  * @src: string to be concatenated to dest
+  * Return: string dest+ n letters of src
+  */
+char *_strncat(char *dest, char *src, int n)
+{
+	int len = 0;
+	int i = 0;
+
+	while (dest[len] != '\0')
+		len++;
+
+	while ((i < n) && (*src != '\0'))
+	{
+		dest[len + i] = *src;
+		src++;
+		i++;
+	}
+	dest[len + i] = '\0';
+
+	return (dest);
+}
