1e12959550fdf80843fef5be17263751f4bdbc00
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-06-29 06:01:45 -0700

    .

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * cap_string - make all first characters in words Uppercase
+ * @str: string
+ *
+ * Return: string
+ */
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[index] != '\0')
+	{
+		if (index == 0)
+		{
+			if (str[index] >= 'a' && str[index] <= 'z')
+				str[index] -= 32;
+		} else if (index > 0 && (str[index - 1] == ' '
+					|| str[index - 1] == '\n'
+					|| str[index - 1] == ','
+					|| str[index - 1] == ';'
+					|| str[index - 1] == '.'
+					|| str[index - 1] == '!'
+					|| str[index - 1] == '?'
+					|| str[index - 1] == '\"'
+					|| str[index - 1] == '('
+					|| str[index - 1] == ')'
+					|| str[index - 1] == '{'
+					|| str[index - 1] == '}')
+		{
+			if (str[index] >= 'a' && str[index] <= 'z')
+				str[index] -= 32;
+		}
+		index++;
+	}
+
+	return (str);
+}
