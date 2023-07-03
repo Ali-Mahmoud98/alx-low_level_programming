b0645118dfc90c3c2b67b2ee71970e054b5ffdf3
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-07-03 16:37:36 -0700

    task 5 done

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		char *i = haystack;
+		char *j = needle;
+
+		while (*i == *j && *i)
+		{
+			i++;
+			j++;
+		}
+
+		if (*j == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
+
