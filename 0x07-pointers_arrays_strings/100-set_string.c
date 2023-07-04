4fd51a65b588756f0c3d95efa9d3947c16ef0f54
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-07-03 17:20:31 -0700

    task 8 done

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to pointer
+ * @to: pointer char
+ */
+void set_string(char **s, char *to)
+{
+	/* '*s' will point to location of 'to'*/
+	*s = to;
+	/*for char* s1 = "bla" and char* s2 = "foo"*/
+	
+	/* the &s1 means the location that the pointer that points
+	 * to first char in s.
+	 * pass sting to first param like that (&s1) => (**&s1) => (*s1)
+	 */
+}
+
