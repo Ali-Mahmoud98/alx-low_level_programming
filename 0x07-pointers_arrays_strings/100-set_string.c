a1b50dcd20ab35db0886e242abb9998088f5f6b8
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-07-03 17:23:09 -0700

    betty issue fixed

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -10,10 +10,9 @@ void set_string(char **s, char *to)
 	/* '*s' will point to location of 'to'*/
 	*s = to;
 	/*for char* s1 = "bla" and char* s2 = "foo"*/
-	
-	/* the &s1 means the location that the pointer that points
-	 * to first char in s.
-	 * pass sting to first param like that (&s1) => (**&s1) => (*s1)
-	 */
+
+	/* the &s1 means the location that the pointer that points*/
+	/* to first char in s.*/
+	/* pass sting to first param like that (&s1) => (**&s1) => (*s1)*/
 }
 
