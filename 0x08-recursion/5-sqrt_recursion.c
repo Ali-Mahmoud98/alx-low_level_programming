88c57938aacb269a88950019bb961e31573ef118
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-07-05 09:27:25 -0700

    task 5 done

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,7 +12,7 @@ int sqrt_check(int n, int i)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return sqrt_check(n, i + 1);
+	return (sqrt_check(n, i + 1));
 }
 /**
  * _sqrt_recursion - returns the natural square root of a number
