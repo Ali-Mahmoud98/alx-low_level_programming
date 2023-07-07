86e1c3dccde55881a4fc5497a51388ff46684f9b
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-07-07 12:42:30 -0700

    task 1 done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - entry point
+  * @argc: number of command line arguments
+  * __attribute__((unused)) means that the arguments is unused
+  * @argv: pointer to array of characters, [array of strings]
+  * argv is array of arguments.
+  *
+  * Return: 0 Always Success
+  */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
