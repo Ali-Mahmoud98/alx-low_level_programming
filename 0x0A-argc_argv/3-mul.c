96704313598cf5d5f9deddf826f0dc15ab93c0ce
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-07-07 13:03:59 -0700

    task 2 done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - entry point
+  * @argc: number of command line arguments
+  * @argv: pointer to array of characters, [array of strings]
+  * argv is array of arguments.
+  *
+  * Return: 0 Always Success
+  */
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		int i = 1;
+		int mul = 1;
+
+		for (; i < argc; i++)
+			mul *= atoi(argv[i]);
+
+		printf("%d\n", mul);
+	} else
+		printf("Error\n");
+
+	return (0);
+}
