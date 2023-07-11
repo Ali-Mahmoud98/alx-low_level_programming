9af8bedef1c93bc392577f20687b8b1577ef8c76
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-07-11 11:12:51 -0700

    second advanced task done

---
+++ 0x0B-malloc_free/101-main.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_tab - Prints an array of string
+ * @tab: The array to print
+ *
+ * Return: nothing
+ */
+void print_tab(char **tab)
+{
+    int i;
+
+    for (i = 0; tab[i] != NULL; ++i)
+    {
+        printf("%s\n", tab[i]);
+    }
+}
+/**
+  * free_p2p2char - free the dynamic allocated array of char
+  * @tab: array of strings
+  */
+void free_p2p2char(char **tab)
+{
+    int i;
+
+    if (tab == NULL)
+        return;
+
+    for (i = 0; tab[i] != NULL; ++i)
+    {
+        free(tab[i]);
+    }
+
+    free(tab);
+}
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: 1 if an error occurred, 0 otherwise
+ */
+int main(void)
+{
+    char **tab;
+
+    tab = strtow("      ALX School         #cisfun      ");
+    if (tab == NULL)
+    {
+        printf("Failed\n");
+        return (1);
+    }
+    print_tab(tab);
+    free_p2p2char(tab);
+    return (0);
+}
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,66 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
+ * @i: pointer to interger
+ * Return: number of words and length of s
+ */
+int count_word(char *s, int *i)
+{
+	int words = 0;
+
+	*i = 0;
+	while (s[*i] != '\0')
+	{
+		if (s[*i] != ' ' &&
+				(s[(*i) + 1] == ' ' || s[(*i) + 1] == '\0'))
+			words++;
+		(*i)++;
+	}
+	return (words);
+}
+
+/**
+  * strtow - splits a string into words
+  * @str: string
+  * Return: array of strings or NULL if str == NULL or str == ""
+  */
+char **strtow(char *str)
+{
+	int len, x, y, i = 0;
+	int words = count_word(str, &len);
+	char **arr = (char **) malloc(sizeof(char *) * (words + 1));
+
+	if (words == 0)
+		return (NULL);
+	if (arr == NULL)
+		return (NULL);
+	for (x = 0; x < words; x++)
+	{
+		int c = 0;
+
+		arr[x] = (char *) malloc(sizeof(char) + 1);
+		if (arr[x] == NULL)
+			return (NULL);
+		for (y = i; y <= len; y++)
+		{
+			if (str[y] != ' ' && str[y] != '\0')
+			{
+				arr[x][c] = str[y];
+				c++;
+				arr[x] = (char *) realloc(arr[x], (c + 1));
+				if (str[y + 1] == ' ' || str[y + 1] == '\0')
+				{
+					arr[x][c] = '\0';
+					i++;
+					break;
+				}
+			}
+			i++;
+		}
+	}
+	arr[words] = NULL;
+	return (arr);
+}
