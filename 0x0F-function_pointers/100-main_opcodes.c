a65849e646f08c61af2e9ee38e281bec25754480
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-07-19 05:45:57 -0700

    .

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+  * main - program that prints the opcodes of its own main function
+  *
+  * @argc: int
+  * @argv: array of strings
+  *
+  * Return: 0 means success
+  */
+int main(int argc, char *argv[])
+{
+	int bytes;
+	char *ptr;
+	int i = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	ptr = (char *) main;
+	for (; i < bytes; i++)
+		printf("%02hhx ", arr[i]);
+
+	printf("\n");
+	return (0);
+}
