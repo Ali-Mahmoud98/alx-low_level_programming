0d2fdab8042faae02ce932803a1380f0306c9124
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-08-02 20:45:38 +0300

    segmentation fault fixed

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,10 +15,19 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	if (newNode == NULL)
 		return (NULL);
-	while (cur->next != NULL)
-		cur = cur->next;
+	
 	newNode->n = n;
 	newNode->next = NULL;
+	
+	if (*head == NULL)
+	{
+		*head = newNode;
+		return (newNode);
+	}
+
+	while (cur->next != NULL)
+		cur = cur->next;
+	
 	cur->next = newNode;
 
 	return (newNode);
