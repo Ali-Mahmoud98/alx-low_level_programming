575fd878b622df966a8e16a78504a42bea03db9f
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-08-02 20:20:51 +0300

    betty style problem solved

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,8 +10,8 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t* newNode = malloc(sizeof(listint_t));
-	
+	listint_t *newNode = malloc(sizeof(listint_t));
+
 	if (newNode == NULL)
 		return (NULL);
 
