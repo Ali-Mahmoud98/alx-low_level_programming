9127e18096988bd56d10cb5f88dedd061f837cbd
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-08-03 21:36:02 +0300

    Floyd's Tortoise and Hare algorithm

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -11,7 +11,7 @@ listint_t *find_listint_loop(listint_t *head)
 	listint_t *slow = head;
 	listint_t *fast = head;
 
-	if (head != NULL)
+	if (head == NULL)
 		return (NULL);
 
 	while (slow && fast && fast->next)
