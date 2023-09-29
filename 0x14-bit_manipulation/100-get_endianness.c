2ccb1876742e4efc6ff76d45f3de4ffa8734d5b3
ali-mahmoud98 <ali98mahmoudr@gmail.com>
2023-09-29 12:18:34 +0300

    .

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness.
+ * Return: 0 for big, 1 for little
+ */
+
+int get_endianness(void)
+{
+	unsigned int indainness = 1;
+	char *ptr = (char *) &indainness;
+	return (*ptr);
+}
