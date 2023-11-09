ed76d890802b108660dabe5330bc05b4e8c6a479
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-11-09 22:01:01 +0200

    .

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,47 +1,50 @@
 #include "lists.h"
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position.
- * @h: pointer to dlistint_t.
- * @idx: index of node.
- * @n: integer.
+ * insert_dnodeint_at_index - inserts a new node at
+ * a given position
+ *
+ * @h: head of the list
+ * @idx: index of the new node
+ * @n: value of the new node
  * Return: the address of the new node, or NULL if it failed
-*/
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *cur = *h;
-	dlistint_t *new = malloc(sizeof(dlistint_t));
-	unsigned int count = 0;
+    dlistint_t *new;
+    dlistint_t *cur;
+    unsigned int count;
 
-	if (*h == NULL)
-		add_dnodeint(h, n);
-	
-	if (!new)
-		return (NULL);
-	new->n = n;
-	if (idx == 0)
-	{
-		new->next = *h;
-		new->prev = NULL;
-		*h = new;
-		return (new);
-	}
-	while (cur)
-	{
-		if (count == idx)
-		{
-			new->next = cur;
-			new->prev = cur->prev;
-			cur->prev->next = new;
+    if (idx == 0)
+    {
+        new = malloc(sizeof(dlistint_t));
+        if (new == NULL)
+            return (NULL);
+        new->n = n;
+        new->next = *h;
+        new->prev = NULL;
+        if (*h != NULL)
+            (*h)->prev = new;
+        *h = new;
+        return (new);
+    }
+    cur = *h;
+    count = 0;
+    while (cur != NULL)
+    {
+        if (count == idx)
+        {
+            new = malloc(sizeof(dlistint_t));
+            if (new == NULL)
+                return (NULL);
+            new->n = n;
+            new->next = cur;
+            new->prev = cur->prev;
+            cur->prev->next = new;
 			cur->prev = new;
-			return (new);
-		}
-		if (cur->next == NULL)
-		{
-			add_dnodeint_end(h, n);
-			return (new);
-		}
-		cur = cur->next;
-		count++;
-	}
-	return (NULL);
+            return (new);
+        }
+        cur = cur->next;
+        count++;
+    }
+    return (NULL);
 }
