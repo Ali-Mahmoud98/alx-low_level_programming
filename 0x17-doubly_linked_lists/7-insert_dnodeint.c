c2585edc1048c7a292bee1c990ec43143c9bf15d
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-11-09 22:22:47 +0200

    .

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * insert_dnodeint_at_index - inserts a new node at
  * a given position
@@ -10,37 +11,42 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new = NULL;
-	dlistint_t *cur;
-	unsigned int count;
+	dlistint_t *new;
+	dlistint_t *head;
+	unsigned int i;
 
+	new = NULL;
 	if (idx == 0)
 		new = add_dnodeint(h, n);
 	else
 	{
-		cur = *h;
-		count = 0;
-		while (cur != NULL)
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
 		{
-			if (count == idx)
+			if (i == idx)
 			{
-				if (cur->next == NULL)
+				if (head->next == NULL)
 					new = add_dnodeint_end(h, n);
 				else
 				{
 					new = malloc(sizeof(dlistint_t));
-					if (new == NULL)
-						return (NULL);
-					new->n = n;
-					new->next = cur;
-					new->prev = cur->prev;
-					cur->prev->next = new;
-					cur->prev = new;
+					if (new != NULL)
+					{
+						new->n = n;
+						new->next = head->next;
+						new->prev = head;
+						head->next->prev = new;
+						head->next = new;
+					}
 				}
 				break;
 			}
-			cur = cur->next;
-			count++;
+			head = head->next;
+			i++;
 		}
 	}
 	return (new);
