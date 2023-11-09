4477c815c8d7e24ad92115e58428988555c22090
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-11-09 20:37:43 +0200

    .

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -3,6 +3,7 @@
  * sum_dlistint - function that returns the sum of all the
  * data (n) of a dlistint_t linked list.
  *
+ * @head: pointer to dlistint_t.
  * Return: sum or if the list is empty, return 0.
 */
 int sum_dlistint(dlistint_t *head)
