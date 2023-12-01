fe716ee39ae0f39d06c9b4d7ec7a4570076b55f0
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-12-01 18:18:06 +0200

    .

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,7 +13,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *node;
 
 	if (ht == NULL || key == NULL || *key == '\0')
-		return NULL;
+		return (NULL);
 	indx = key_index((const unsigned char *) key, ht->size);
 	if (indx >= ht->size)
 		return (NULL);
