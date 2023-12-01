d8446c006d52d4d791b0a0f944f99bae2b377773
Ali-Mahmoud98 <ali98mahmoudr@gmail.com>
2023-12-01 15:04:56 +0200

    .

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -20,7 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_t->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_t->array == NULL)
 	{
-		free(hash_t);
+		/*free(hash_t);*/
 		return (NULL);
 	}
 
